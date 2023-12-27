c99af4cf91a1caed15273448d4ee295a20b8746b
wenghengcong <wenghengcong@jd.com>
2023-12-27 18:26:13 +0800

    add

--- HCUtilKit/Sources/HCUtilKit/Ext/Date+Ext.swift
+++ HCUtilKit/Sources/HCUtilKit/Ext/Date+Ext.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// 特殊日期
 public extension Date {
     func startOfMonth(using calendar: Calendar) -> Date {
         calendar.date(
@@ -15,20 +16,87 @@ public extension Date {
     }
 }
 
-public extension DateFormatter {
-    convenience init(dateFormat: String, calendar: Calendar) {
-        self.init()
-        self.dateFormat = dateFormat
-        self.calendar = calendar
-    }
-}
+/*
+ let dateTest = Date()
+ let p1 = dateTest.utcNoMill
+ let p2 = dateTest.utc
+ let p3 = dateTest.chinaNoMill
+ let p4 = dateTest.china
 
+ print(p1)
+ print(p2)
+ print(p3)
+ print(p4)
+
+ let d1 = Date(iso: p1)
+ let d2 = Date(iso: p2)
+ let d3 = Date(iso: p3)
+ let d4 = Date(iso: p4)
+
+ print(d1)
+ print(d2)
+ print(d3)
+ print(d4)
+ */
 public extension Date {
-    var dayStr: String {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.dateFormat = "dd"
-        return formatter.string(from: self)
+    
+    /// utc时区 格式的字符串
+    /// 格式：2023-12-27T05:50:56Z
+    var utcNoMill: String {
+        return DateFormatter.utcNoMill.string(from: self)
+    }
+    
+    var utc: String {
+        return DateFormatter.utc.string(from: self)
+    }
+    
+    /// 中国时区无毫秒
+    var chinaNoMill: String {
+        return DateFormatter.chinaNoMill.string(from: self)
+    }
+    
+    var china: String {
+        return DateFormatter.china.string(from: self)
+    }
+    
+   
+    
+    /// 以UTC时区新建Date
+    /// - Parameter utcString: 格式：2023-12-27T05:50:56Z
+    init?(iso: String, timeZone: String = "UTC") {
+        
+        let timeZone = TimeZone(identifier: timeZone)
+        let utcFomatter = DateFormatter.utc
+        let utcNoMillFomatter = DateFormatter.utc
+        utcFomatter.timeZone = timeZone
+        utcNoMillFomatter.timeZone = timeZone
+        
+        var parseDate: Date? = nil
+        if let date = utcFomatter.date(from: iso) {
+            parseDate = date
+        } else {
+            if let date = utcNoMillFomatter.date(from: iso) {
+                parseDate = date
+            }
+        }
+        if let date = parseDate {
+            self = date
+        } else {
+            return nil
+        }
+    }
+
+    /// 返回ISO8601标准格式的字符串
+    /// - Returns: "2023-12-26T15:39:10.694+08:00"
+    func iso(mill: Bool = true, china: Bool = false) -> String {
+        var isoDateFormmater: ISO8601DateFormatter
+        if(china) {
+            isoDateFormmater = mill ? .china : .chinaNoMill
+        } else {
+            isoDateFormmater = mill ? .utc : .utcNoMill
+        }
+        let dateStr: String = isoDateFormmater.string(from: self)
+        return dateStr
     }
     
     static func getDate(from dateString: String) -> Date? {
--- HCUtilKit/Sources/HCUtilKit/Ext/DateFormatter+Ext.swift
+++ HCUtilKit/Sources/HCUtilKit/Ext/DateFormatter+Ext.swift
@@ -1,13 +1,83 @@
 //
-//  DateFormatter.swift
+//  DateFormatter+Ext 2.swift
+//  
 //
-//
-//  Created by Nemo on 2023/10/27.
+//  Created by Nemo on 2023/12/27.
 //
 
 import Foundation
 
+public extension ISO8601DateFormatter {
+    /// iso8601 默认无毫秒，没有设置formatOptions
+    static let iso8601: ISO8601DateFormatter = ISO8601DateFormatter()
+    
+    
+    
+    /// UTC：不包含毫秒
+    /// 格式：2023-12-27T05:50:56Z
+    static var utcNoMill: ISO8601DateFormatter = iso8601(millsec: false, china: false)
+    
+    /// UTC
+    /// e.g. 2023-12-27T05:50:56.694Z
+    static var utc: ISO8601DateFormatter = iso8601(millsec: true, china: false)
+
+    /// 中国时区的iso8601
+    /// e.g. "2023-12-26T15:39:10+08:00"
+    static var chinaNoMill: ISO8601DateFormatter = iso8601(millsec: false, china: true)
+
+    /// 中国时区的iso8601
+    /// e.g. "2023-12-26T15:39:10.694+08:00"
+    static var china: ISO8601DateFormatter = iso8601(millsec: true, china: true)
+
+    /// iso8601标准格式
+    /// - Parameters:
+    ///   - millsec: 是否含有毫秒时间
+    ///   - china: 时区是否设置为中国，true是中国时区，false是UTC
+    /// - Returns: "2023-12-26T15:39:10.694+08:00"
+    static func iso8601(millsec: Bool = true,
+                        china: Bool = false) -> ISO8601DateFormatter {
+        let dateFormatter = ISO8601DateFormatter()
+        if millsec {
+            dateFormatter.formatOptions = [.withInternetDateTime,.withFractionalSeconds, .withTimeZone]
+        } else {
+            dateFormatter.formatOptions = [.withInternetDateTime, .withTimeZone]
+        }
+        dateFormatter.timeZone = china ? .Shanghai : .utc
+        return dateFormatter
+    }
+}
+
 public extension DateFormatter {
+    
+    /// iso8601 没有设置formatOptions
+    static let iso8601: ISO8601DateFormatter = .iso8601
+    
+    /// UTC：不包含毫秒
+    /// 格式：2023-12-27T05:50:56Z
+    static var utcNoMill: ISO8601DateFormatter = .utcNoMill
+    
+    /// UTC
+    /// e.g. 2023-12-27T05:50:56.694Z
+    static var utc: ISO8601DateFormatter = .utc
+
+    /// 中国时区的iso8601
+    /// e.g. "2023-12-26T15:39:10+08:00"
+    static var chinaNoMill: ISO8601DateFormatter = .chinaNoMill
+
+    /// 中国时区的iso8601
+    /// e.g. "2023-12-26T15:39:10.694+08:00"
+    static var china: ISO8601DateFormatter = .china
+    
+    convenience init(dateFormat: String, calendar: Calendar) {
+        self.init()
+        self.dateFormat = dateFormat
+        self.calendar = calendar
+    }
+    
+    /// ISO8601标准格式
+    /// - Parameter china: 是否是中国时区
+    /// - Returns: ISO8601DateFormatter
+    
     static var month: DateFormatter {
         let formatter = DateFormatter()
         formatter.dateFormat = "MMMM"
